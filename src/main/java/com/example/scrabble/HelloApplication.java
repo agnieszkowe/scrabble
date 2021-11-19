d799e2934b01e584c92ba5a7afa8e452a3c11d1c
kowaleuro <kowaleuro@gmail.com>
2021-11-19 20:08:16 +0100

    literki i giveUpTheWord

--- src/main/java/com/example/scrabble/HelloApplication.java
+++ src/main/java/com/example/scrabble/HelloApplication.java
@@ -1,10 +1,14 @@
 package com.example.scrabble;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,21 +19,96 @@ public class HelloApplication extends Application {
 
     public ArrayList<Field> fieldArrayList = new ArrayList<>();
     public ArrayList<Letter> letterArrayList = new ArrayList<>();
+    public ArrayList<Player> playerArrayList = new ArrayList<>();
+    public ArrayList<LetterField> letterFieldArrayList = new ArrayList<>();
+    public Player player;
+    private Button giveBackWord;
     @Override
     public void start(Stage stage) throws IOException {
         Pane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/scrabble/hello-view.fxml")));
         stage.setScene(new Scene(root));
         stage.setTitle("Hello!");
         stage.show();
+        giveBackWord = new Button("Give bACK THE WORD");
+        giveBackWord.relocate(600,600);
+        root.getChildren().add(giveBackWord);
         Generator generator = new Generator();
         fieldArrayList = generator.mapGenerator(root);
-        generator.LetterFieldsGenerator(root);
+        letterFieldArrayList = generator.LetterFieldsGenerator(root);
         letterArrayList = generator.LetterGenerator();
-        ArrayList<Letter> player1LetterArrayList = generator.PlayerLetterRandom(letterArrayList);
-        ArrayList<Letter> player2LetterArrayList = generator.PlayerLetterRandom(letterArrayList);
+        playerArrayList.add(new Player("Aga",generator.PlayerLetterRandom(letterArrayList)));
+        playerArrayList.add(new Player("Maks",generator.PlayerLetterRandom(letterArrayList)));
+        player = playerArrayList.get(0);
+        setLettersOfPlayer(player.playersLetters);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), e -> gameLoop()));
+                timeline.setCycleCount(Timeline.INDEFINITE);
+                timeline.play();
+                //setTimeline(timeline);
+            }
+        }).start();
+    }
+
+    private void gameLoop() {
+        //System.out.println("siema");
+        if (this.giveBackWord.isPressed()){
+            nextTurnGenerator();
+        }
+        setLetterFieldTouch();
+        checkIfLetterInput();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+
+    private void nextTurnGenerator(){
+        playerArrayList.remove(player);
+        playerArrayList.add(player);
+        this.player = playerArrayList.get(0);
+        setLettersOfPlayer(player.playersLetters);
+
+
+    }
+
+    private void setLettersOfPlayer(ArrayList<Letter> letters){
+        int counter = 0;
+        for (Letter letter:letters) {
+            letterFieldArrayList.get(counter).button.setText(letter.getLetter());
+            counter++;
+        }
+
+    }
+
+    private void checkIfLetterInput(){
+        for (LetterField letterField:letterFieldArrayList) {
+            if (letterField.isTouched()){
+                for (Field field:fieldArrayList) {
+                    if (field.button.isPressed()){
+                        System.out.println("hello");
+                        field.button.setText(letterField.button.getText());
+                        field.button.setDisable(true);
+                        letterField.button.setDisable(true);
+                        letterField.setTouched(false);
+                    }
+                }
+            }
+        }
+    }
+
+    private void setLetterFieldTouch(){
+        for (LetterField letterField:letterFieldArrayList) {
+            if (letterField.button.isPressed()){
+                letterField.setTouched(true);
+                for (LetterField letterToFalse:letterFieldArrayList) {
+                    if(letterToFalse != letterField){
+                        letterToFalse.setTouched(false);
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/com/example/scrabble/LetterField.java
+++ src/main/java/com/example/scrabble/LetterField.java
@@ -3,7 +3,17 @@ package com.example.scrabble;
 import javafx.scene.layout.Pane;
 
 public class LetterField extends Field{
-    public LetterField(double x, double y, String color, Pane layer,String letter) {
+    private boolean isTouched;
+
+    public boolean isTouched() {
+        return isTouched;
+    }
+
+    public void setTouched(boolean touched) {
+        isTouched = touched;
+    }
+
+    public LetterField(double x, double y, String color, Pane layer, String letter) {
         super(x, y, color, layer);
         this.button.setText(letter);
         this.button.setPrefSize(40,40);
